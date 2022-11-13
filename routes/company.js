a2be0baddc50e18816d8d6edb56424b16bfc042e
abhirami <abhiabhirami972@gmail.com>
2022-11-13 22:51:24 +0530

    completed routes

--- routes/company.js
+++ routes/company.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { company, signup } = require("../controllers/companycontroler")
+const { company, signup, login } = require("../controllers/companycontroler")
 
 
 router.get("/", company)
 router.get("/signup", signup)
+router.get("/login",login)
 
 
 module.exports = router;
