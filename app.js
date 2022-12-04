d95af3a1bc3ab033676c0a93847159a2eeaddef6
abhirami <abhiabhirami972@gmail.com>
2022-12-04 20:56:43 +0530

    completed view job

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload())
+app.use(fileUpload());
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
